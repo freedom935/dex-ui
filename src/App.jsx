5a26c17a2546544e0fa6c7ace5ced54c1a52161e
vic <vic@gmail.com>
2024-11-09 13:01:30 +0330

    NotFoundPage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import TermsPage from "./pages/TermsPage"
+import NotFoundPage from "./pages/NotFoundPage"
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />
+    element: <HomePage />,
+    errorElement: <NotFoundPage />
   },
   {
     path: "/terms",
