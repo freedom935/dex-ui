1946652529441d4f0700bfe6e87fecc353853dc0
vic <vic@gmail.com>
2024-11-09 11:40:51 +0330

    NavBar & TermsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import TermsPage from "./pages/TermsPage"
+
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/terms" element={<TermsPage />} />
     </Routes>
   )
 }
